c0f12c61044de5d039eb191267ae9c3581b5e431
kazutoiris <78157415+kazutoiris@users.noreply.github.com>
2021-05-25 16:34:57 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # DO NOT DELETE
-echo $FLAG > /home/ctf/bin/flag && export FLAG=not_flag && FLAG=not_flag
-/etc/init.d/xinetd start;
-sleep infinity;
+echo $FLAG >/home/ctf/bin/flag
+echo $FLAG >/home/ctf/flag
+export FLAG=not_flag && FLAG=not_flag
+/etc/init.d/xinetd start
+sleep infinity
